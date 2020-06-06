7e2d202b28a98dab330ee1f1c4d0421e88e1597f
Renato Bianco <renato_bianco@apple.com>
2020-06-06 17:56:46 +0100

    uploading files

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/breakfast')
+def breakfast():
+    return render_template("breakfast.html")
+
+
+@app.route('/lunch')
+def lunch():
+    return render_template("lunch.html")
+
+
+@app.route('/dinner')
+def dinner():
+    return render_template("dinner.html")
+
+
+@app.route('/dessert')
+def dessert():
+    return render_template("dessert.html")
+
+
+@app.route('/drinks')
+def drinks():
+    return render_template("drinks.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
