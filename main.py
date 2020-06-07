88e4e395a315b58c2e531774e9364b830543b3d2
Renato Bianco <renato_bianco@apple.com>
2020-06-07 19:14:10 +0100

    adding app.run vars in main.py for Heroku

--- main.py
+++ main.py
@@ -41,5 +41,13 @@ def contact():
 def profile():
     return render_template("profile.html")
 
+
+if __name__ =="__main__":
+    app.run(host=os.getenv("IP"),
+       port=int(os.getenv("PORT")),
+       debug=True)
+       
+"""
 if __name__ == "__main__":
   app.run(debug=True)
+"""
