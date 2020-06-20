0b3d9164da107889bb51300f1984b188a4a1f96e
Renato Bianco <renato_bianco@apple.com>
2020-06-20 20:09:35 +0100

    adding main.py for heroku

--- main.py
+++ main.py
@@ -49,14 +49,15 @@ def profile():
 
 
 
-"""
+
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
        port=int(os.getenv("PORT")),
        debug=True)
+
+
+
 """
-
-
-
 if __name__ == "__main__":
   app.run(debug=True)
+"""
