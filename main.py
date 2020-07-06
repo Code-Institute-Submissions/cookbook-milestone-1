8728bc851d5ff0d37bf425e59094119e52789b6f
Renato Bianco <renato_bianco@apple.com>
2020-07-06 17:12:42 +0100

    completed add.html form and now form is connected to MongoDB, add.html is now able to upload recipes, repice image will stay in local, renamed with the record's ObjectID

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
-from flask import Flask, render_template
+import datetime
+from flask import Flask, flash, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+# app.config["IMAGE_UPLOADS"] = 'images'
+app.config["MONGO_DBNAME"] = 'Cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://codeinstitute:c0deinstitute@codeinstitute.jmzvl.mongodb.net/cookbook?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -41,7 +50,7 @@ def contact():
 
 @app.route('/recipe')
 def recipe():
-    return render_template("recipe.html")
+    return render_template("recipe.html", recipes=mongo.db.recipes.find())
 
 
 @app.route('/profile')
@@ -51,13 +60,38 @@ def profile():
 
 @app.route('/add')
 def add():
-    return render_template("add.html")
+    return render_template("add.html", recipes=mongo.db.recipes.find())
 
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    
+    x = datetime.datetime.now()
+    posted = x.strftime("%d") + " " + x.strftime("%b") + " " + x.strftime("%Y")
+
+    recipes = mongo.db.recipes
+    # recipes.insert_one(request.form.to_dict())
+
+    dict1 = request.form.to_dict()
+    dict1['posted'] = posted
+
+    recipes.insert_one(dict1)
+
+    if request.files:
+
+        image = request.files["picture"]
+        ext = image.filename.rsplit(".", 1)[1]
+        path = os.getcwd()
+        image_dir = path + "/static/images/" + str(dict1["_id"]) + "." + ext
+        #image_dir = path + "/static/images/" + image.filename
+
+        image.save(image_dir)
+
+    # Once that's done, we redirect to recipe.html, so we can view the new recipe in our collection.
+    return redirect(url_for('index'))
 
 
-
-
+"""
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
        port=int(os.getenv("PORT")),
@@ -65,7 +99,5 @@ if __name__ =="__main__":
 """
 
 
-
 if __name__ == "__main__":
-  app.run(debug=True)
-"""
+    app.run(debug=True)
