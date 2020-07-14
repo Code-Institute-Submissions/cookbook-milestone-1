7f52b582acb0654fbed48877a0cb63e6b32984c1
Renato Bianco <renato_bianco@apple.com>
2020-07-14 19:14:47 +0100

    added buttons edit and delete on recipe.htm, created edit_recipe.html. Create, Read, Update and Delete are now completed.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+
 app = Flask(__name__)
 
 # app.config["IMAGE_UPLOADS"] = 'images'
@@ -48,47 +49,111 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route('/recipe')
-def recipe():
-    return render_template("recipe.html", recipes=mongo.db.recipes.find())
+@app.route('/recipe/<recipe_id>')
+def recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("recipe.html", recipe=the_recipe)
 
 
-@app.route('/profile')
-def profile():
-    return render_template("profile.html")
+@app.route('/delete_task/<recipe_id>')
+def delete_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    oldimg = str(the_recipe['imgfullname'])
+    path = os.getcwd()
+    oldimgpath = path + "/static/images/" + oldimg
+    os.remove(oldimgpath)
+
+    mongo.db.recipes.delete_one({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('index'))
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('edit_recipe.html', recipe=the_recipe)
 
 
-@app.route('/add')
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+
+    recipes.update_one({'_id': ObjectId(recipe_id)}, {"$set":
+        {
+            "title": request.form.get('title'),
+            "category_name": request.form.get('category_name'),
+            "ptime": request.form.get('ptime'),
+            "ctime": request.form.get('ctime'),
+            "serves": request.form.get('serves'),
+            "slevel": request.form.get('slevel'),
+            "ingredients": request.form.get('ingredients'),
+            "method": request.form.get('method')
+        }})
+    
+    if request.files["picture"]:
+        the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+        oldimg = str(the_recipe['imgfullname'])
+        path = os.getcwd()
+        oldimgpath = path + "/static/images/" + oldimg
+        os.remove(oldimgpath)
+
+        image = request.files["picture"]
+        ext = image.filename.rsplit(".", 1)[1]
+
+        image_dir = path + "/static/images/" + str(ObjectId(recipe_id)) + "." + ext
+        image.save(image_dir)
+        newpicture = str(recipe_id) + "." + ext
+        
+        recipes.update_one({'_id': ObjectId(recipe_id)}, {"$set":
+        {
+            "imgfullname": newpicture
+        }})
+
+    return redirect(url_for('recipe', recipe_id=recipe_id))
+
+
+@ app.route('/add')
 def add():
     return render_template("add.html", recipes=mongo.db.recipes.find())
 
 
-@app.route('/insert_recipe', methods=['POST'])
+@ app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    
-    x = datetime.datetime.now()
-    posted = x.strftime("%d") + " " + x.strftime("%b") + " " + x.strftime("%Y")
 
-    recipes = mongo.db.recipes
+    x=datetime.datetime.now()
+    posted=x.strftime("%d") + " " + x.strftime("%b") + " " + x.strftime("%Y")
+
+    recipes=mongo.db.recipes
     # recipes.insert_one(request.form.to_dict())
 
-    dict1 = request.form.to_dict()
-    dict1['posted'] = posted
+    dict1=request.form.to_dict()
+    dict1['posted']=posted
+    dict1['voters']=0
+    dict1['votesum']=0
+    dict1['imgfullname']=0
 
     recipes.insert_one(dict1)
 
-    if request.files:
+    image=request.files["picture"]
+    ext=image.filename.rsplit(".", 1)[1]
+    path=os.getcwd()
+    image_dir=path + "/static/images/" + str(dict1["_id"]) + "." + ext
 
-        image = request.files["picture"]
-        ext = image.filename.rsplit(".", 1)[1]
-        path = os.getcwd()
-        image_dir = path + "/static/images/" + str(dict1["_id"]) + "." + ext
-        #image_dir = path + "/static/images/" + image.filename
+    image.save(image_dir)
 
-        image.save(image_dir)
+    imgfullname=str(dict1["_id"]) + "." + ext
 
-    # Once that's done, we redirect to recipe.html, so we can view the new recipe in our collection.
-    return redirect(url_for('index'))
+
+    myquery={"_id": dict1["_id"]}
+    newvalues={"$set": {"imgfullname": imgfullname}}
+
+    recipes.update_one(myquery, newvalues)
+
+    takeid=str(dict1["_id"])
+
+    return redirect(url_for('recipe', recipe_id=takeid))
+
+
+
+
 
 
 """
